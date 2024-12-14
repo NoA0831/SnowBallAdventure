6cc14abbaedcc33b2ebb4c08bfa0705e2b902078
NoA <noa20040831@gmail.com>
2024-12-14 23:04:09 +0900

    画面外に出たかを判定する条件式が間違っていたのを更新

---
+++ SnowBallAdventure/isOutWindow.cpp
@@ -0,0 +1,10 @@
+﻿#pragma once
+#include "Common.hpp"
+
+static bool isOutWindow(const Vec2& pos) {
+
+	if (0 > pos.x || pos.x > Scene::Width() || 0 > pos.y || pos.y > Scene::Height()) {
+		return true;
+	}
+	return false;
+}
